eca8a87f1ab20f2903cb5fdf2b654b4bceb5fc64
gyeonghun.choi <gyeonghun.choi@playlinks.com>
2025-03-23 18:04:54 +0900

    1.Sleep

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -10,49 +10,29 @@
 class SpinLock
 {
 public:
-	//lock_guard와 인터페이스 같아야 하므로 소문자 lock
 	void lock()
 	{
-		//while (_locked)
-		//{
-
-		//}
-		//_locked = true;
-
-		//CAS로 해결
 		bool expected = false;
 		bool desired = true;
-
-		//의사 코드
-		//if (_locked == expected)
-		//{
-		//	expected = _locked;
-		//	_locked = desired;
-		//	return true;
-		//}
-		//else
-		//{
-		//	expected = _locked;
-		//	return false;
-		//}
 
 		//실패하면 계속 시도해라
 		while (_locked.compare_exchange_strong(expected, desired) == false)
 		{
-			//함수 인자를 레퍼런스로 받아서 값이 바뀌므로 다시 초기값으로 세팅
 			expected = false;
-		}
 
-
+			//이때 동안 대기상태로, 스케줄링 제외
+			//실패 했으면 커널로 돌아간다.
 			
+			//this_thread::sleep_for(chrono::microseconds(100));
+			this_thread::sleep_for(0ms);//operator ms 로 쓸 수 있음
+			//this_thread::yield();//sleep 0ms 와 같음
+		}
 	}
 	void unlock()
 	{
-		//_locked = false;
 		_locked.store(false);//atomic 함수를 사용하여 변경
 	}
 private:
-	//volatile bool _locked = false;//단순 컴파일러 최적화 하지 마라
 	atomic<bool> _locked = false; //volatile 기능을 하고 있다.
 };
 
