ac5d5eb327bd6733effd04d89474ac9604cea0a4
gyeonghun.choi <gyeonghun.choi@playlinks.com>
2025-03-24 23:42:17 +0900

    1.가시성, 코드재배치

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,58 +8,47 @@
 #include <windows.h>
 #include <future>
 
-
-int64 Calculate()
+int32 x;
+int32 y;
+int32 r1;
+int32 r2;
+volatile bool ready;
+void Thread1()
 {
-	int64 sum = 0;
-	for(int32 i = 0; i < 100000; ++i)
-		sum += i;
-	return sum;
+	while (!ready){}
+		
+
+	y = 1;
+	r1 = x;
 }
-
-void PromiseWorker(promise<string>&& promise)
+void Thread2()
 {
-	promise.set_value("MSG");
+	while (!ready){}
+
+	x = 1;
+	r2 = y;
 }
 
 int main()
 {
+	int32 count = 0;
+	while (true)
 	{
-		/*
-		* 1.deferred->lazy evaluation, 지연실행, 그냥 나중에 호출하는거
-		* 2.async->별도 스레드 생성 후 실행
-		* 3.deferred | async->
-		*/
-		future<int64> future = async(launch::async, Calculate);
+		ready = false;
+		++count;
 
-		//1ms만 기다려보겠다, 상태 잠시 확인
-		//future_status status = future.wait_for(1ms);
-		//if (status == future_status::ready)
-		//{
+		x = y = r1 = r2 = 0;
 
-		//}
+		thread t1(Thread1);
+		thread t2(Thread1);
 
-		//계속 기다림, get 하는 거나 똑같음
-		future.wait();
+		ready = true;
 
-		int64 sum = future.get();
+		t1.join();
+		t2.join();
 
-
-		//객체멤버 함수
-		//Knight knight;
-		//future<int64> future2 = async(launch::async, &Knight::GetHp, knight); //knight.GetHp()
-
+		if (r1 == 0 && r2 == 0)
+			break;
 	}
-
-	{
-		//미래에 결과물 반환해줄거라는 약속?
-		promise<string> promise;
-		future<string> future = promise.get_future();
-
-		//future는 메인스레드가 갖고, promise는 워커스레드가 갖는다
-		thread t(PromiseWorker, move(promise));
-		string msg = future.get();
-		cout << msg << endl;
-		t.join();
-	}
+	cout << count << endl;
 }
